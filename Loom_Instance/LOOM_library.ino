6a46c2827b6e568952fec184257852164ec27bfe
goertzel <goertzel@oregonstate.edu>
2018-12-20 12:05:16 -0800

    Clean up .ino

--- Loom_Instance/LOOM_library.ino
+++ Loom_Instance/LOOM_library.ino
@@ -7,8 +7,6 @@
 // Config has to be first has it hold all user specified options
 #include "config.h"
 
-
-
 // Preamble includes any relevant subroutine files based 
 // on options specified in the above config
 #include "loom_preamble.h"
@@ -19,12 +17,8 @@
 // ================================================================ 
 void setup() 
 {
-
-
 	// LOOM_begin calls any relevant (based on config) LOOM device setup functions
 	Loom_begin();	
-
-	// Any custom setup code
 
 }
 
@@ -45,106 +39,6 @@ void loop()
 	delay(1000);
 
 	additional_loop_checks();	// Miscellaneous checks
-
-
-
-
-	// // --- Basic Example ---
-
-	// measure_sensors();				// Read sensors, store data in sensor state struct
-	// package_data(&bndl);			// Copy sensor data from state to provided bundle
-	
-	// print_bundle(&bndl);
-
-	// delay(1000);
-
-	// additional_loop_checks();		// Miscellaneous checks
-
-	// // --- End Example ---
-
-
-
-	// // --- OLED Example ---
-
-	// measure_sensors();			// Read sensors, store data in sensor state struct
-	// package_data(&bndl);			// Copy sensor data from state to provided bundle
-	
-	// print_bundle(&bndl);
-	// log_bundle(&bndl, OLED);
-
-	// delay(1000);
-
-	// additional_loop_checks();	// Miscellaneous checks
-
-	// // --- End Example ---
-
-
-
-	// // --- LoRa Hub Example ---
-
-	// receive_bundle(&bndl, LORA);		// Read sensors, store data in sensor state struct
-	// print_bundle(&bndl);
-	// log_bundle(&bndl, PUSHINGBOX);
-
-	// additional_loop_checks();		// Miscellaneous checks
-	// // --- End Example ---
-
-
-
-	// // --- LoRa Node Example ---
-
-	// measure_sensors();			// Read sensors, store data in sensor state struct
-	// package_data(&bndl);			// Copy sensor data from state to provided bundle
-	
-	// print_bundle(&bndl);
-
-	// log_bundle(&bndl, SDCARD, "savefile.csv");
-	// send_bundle(&bndl, LORA);
-
-	// delay(1000);
-
-	// additional_loop_checks();	// Miscellaneous checks
-	// // --- End Example ---
-
-
-
-	// // --- LoRa Evaporometer Example ---
-
-	// measure_sensors();			// Read sensors, store data in sensor state struct
-	// package_data(&bndl);			// Copy sensor data from state to provided bundle
-	
-	// print_bundle(&bndl);
-
-	// log_bundle(&bndl, SDCARD, "savefile.csv");
-	// send_bundle(&bndl, LORA);
-
-	// sleep_for(5, MINUTES, STANDBY);
-
-	// additional_loop_checks();	// Miscellaneous checks
-	// // --- End Example ---
-
-
-	// --- Common Example ---
-
-//	receive_bundle(&bndl, WIFI);	// Receive messages
-//	if (bndl.size()) {
-//		print_bundle(&bndl);		// Print bundle if LOOM_DEBUG enabled
-//	}
-//	process_bundle(&bndl);			// Dispatch message to correct handling functions
-//
-//	measure_sensors();				// Read sensors, store data in sensor state struct
-//	package_data(&bndl);			// Copy sensor data from state to provided bundle
-//
-//	// print_bundle(&bndl);			// Print bundle if LOOM_DEBUG enabled
-//
-//	send_bundle(&bndl, WIFI);		// Send bundle of packaged data
-//	// log_bundle(&bndl, PUSHINGBOX);	// Send bundle to Google Sheet
-//	// log_bundle(&bndl, SDCARD, "Ishield.csv");	// Send bundle to Google Sheet
-//
-//	additional_loop_checks();			// Miscellaneous checks
-//	// --- End Example ---
-
-
 
 
 }
